9ddf8c2a069c1c9fbe376da7ed896f178c4f91cb
Franco <lmassa.fms@gmail.com>
2024-02-05 15:03:29 -0300

    agrego ejercicio 2, usando props

---
+++ src/components/Saludo.jsx
@@ -0,0 +1,9 @@
+const MyFriend = ({ name }) => {
+    return (
+      <section className="container">
+          <h1 className="text-center my-3 display-2">Hello {name}!</h1>
+      </section>
+    );
+  };
+  
+  export default MyFriend;
